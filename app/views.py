4ac8a64c8f785d1a803094cdecdaca04d5fdd54e
KirtiMutkundu <kirtimutkundureddy2007@gmail.com>
2024-04-07 11:10:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def conditions(request):
+    d={'a':10, 'b':20, 'c':30}
+    return render(request,'conditions.html', d)
+
+
+def loops(request):
+    d={'name':'KIRTI'}
+    return render(request,'loops.html',context=d)
